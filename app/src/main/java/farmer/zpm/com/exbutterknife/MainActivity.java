2a1c5deb8f5645f55de8d97dee04d2edc3a8c7f1
ZhuPumao <20103177@cqu.edu.cn>
2016-06-13 16:12:35 +0800

    Initial commit

---
+++ app/src/main/java/farmer/zpm/com/exbutterknife/MainActivity.java
@@ -0,0 +1,25 @@
+package farmer.zpm.com.exbutterknife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import farmer.zpm.com.exbufferknife.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text)
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        textView.setText("1111111111");
+    }
+
+}
